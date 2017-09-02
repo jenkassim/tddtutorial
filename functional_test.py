c35cf15aa71ad2c43d3fb442a0bd1e89624e6010
jenkassim <jenkassim@gmail.com>
2017-09-02 13:51:27 +0800

    refactor tdd scrit

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,27 @@
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+#browser = webdriver.Firefox()
+#browser.get('http://localhost:8000')
+#assert 'Django' in browser.title
 
-assert 'Django' in browser.title
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown():
+        self.browser.quit()
+
+    def test_can_start_browser(self):
+        self.browser.get('http://localhost:8000')
+    
+        #Page title and header mention to-do list
+        self.assertIn('Django', self.browser.title)
+
+
+if __name__ == '__main__':
+    
+    unittest.main()
+
